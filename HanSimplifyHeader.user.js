ad71068b3c11eb68a2a852a9a809e66b398cdf0e
田生 <tiansheng111@gmail.com>
2022-03-26 16:15:32 +1100

    remove exclude

--- HanSimplifyHeader.user.js
+++ HanSimplifyHeader.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name Han Simplify
 // @name:zh 汉字转换为简体字
-// @description 将页面上的汉字转换为简体字，需要手动添加包含的网站以启用
+// @description 将页面上的汉字转换为简体字
 // @namespace https://github.com/tiansh
-// @version 1.7
+// @version 1.8
 // @resource t2s https://tiansh.github.io/reader/data/han/t2s.json
 // @include *
-// @exclude *
 // @grant GM_getResourceURL
 // @grant GM.getResourceUrl
 // @run-at document-start
--- HanTraditionalizeHeader.user.js
+++ HanTraditionalizeHeader.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name Han Traditionalize
 // @name:zh 漢字轉換為繁體字
-// @description 將頁面上的漢字轉換為繁體字，需要手動添加包含的網站以啟用
+// @description 將頁面上的漢字轉換為繁體字
 // @namespace https://github.com/tiansh
-// @version 1.7
+// @version 1.8
 // @resource s2t https://tiansh.github.io/reader/data/han/s2t.json
 // @include *
-// @exclude *
 // @grant GM_getResourceURL
 // @grant GM.getResourceUrl
 // @run-at document-start
